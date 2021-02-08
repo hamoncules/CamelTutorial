61dcece52999524284866ac3de4a57d375d3a060
Damion Fusco <damionfusco@hotmail.com>
2021-02-07 19:41:45 -0500

    Added src and dst folder read from configuration file

--- src/main/java/ca/yng/camel/file/FileRouter.java
+++ src/main/java/ca/yng/camel/file/FileRouter.java
@@ -1,16 +1,21 @@
 package ca.yng.camel.file;
 
+import ca.yng.camel.configuration.ConfigurationProperties;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.configuration2.Configuration;
 
 public class FileRouter extends RouteBuilder {
 
-    private static final String SOURCE_FOLDER = "src/test/source-folder";
-    private static final String DESTINATION_FOLDER = "src/test/destination-folder";
+    Configuration config = ConfigurationProperties.getConfiguration();
+
+//    private final String fileName = new String(config.getProperty("ca.yng.camel.file.fileName").toString() + (new Date().getTime()));
+    private final String sourceDir = new String(config.getProperty("ca.yng.camel.file.srcDir").toString());
+    private final String destinationDir = new String(config.getProperty("ca.yng.camel.file.dstDir").toString());
 
     @Override
     public void configure() throws Exception {
-        //from("file://" + SOURCE_FOLDER + "?delete=true").process(new FileProcessor()).to("file://" + DESTINATION_FOLDER);
-        from("file://" + SOURCE_FOLDER + "?delete=true").to("file://" + DESTINATION_FOLDER);
+        //from("file://" + sourceDir + "?delete=true").process(new FileProcessor()).to("file://" + destinationDir);
+        from("file://" + sourceDir + "?delete=true").to("file://" + destinationDir);
     }
 
 }
